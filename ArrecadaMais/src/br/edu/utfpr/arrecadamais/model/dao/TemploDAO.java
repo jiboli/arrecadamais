8cede20f44cbe14840beec8dbec2029586ffdc98
jiboli <joao.riboli@outlook.com>
2014-09-25 22:30:35 -0300

    Criacao TemploDao

--- ArrecadaMais/src/br/edu/utfpr/arrecadamais/model/dao/TemploDAO.java
+++ ArrecadaMais/src/br/edu/utfpr/arrecadamais/model/dao/TemploDAO.java
@@ -6,10 +6,52 @@
 
 package br.edu.utfpr.arrecadamais.model.dao;
 
+import br.edu.utfpr.arrecadamais.model.vo.Templo;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 /**
  *
  * @author JoãoHenrique
  */
 public class TemploDAO {
+    public int inserir(Templo templo) throws SQLException{
+        EntityManager manager = ConexaoBD.getInstance()
+                .getEntityManager();
+        
+        manager.getTransaction().begin();
+        manager.persist(templo);
+        manager.getTransaction().commit();
+        
+        return templo.getTemploId();
+        
+    }
+    
+    public void alterar(Templo templo) throws SQLException {
+        EntityManager manager = ConexaoBD.getInstance()
+                .getEntityManager();
+        
+        manager.getTransaction().begin();
+        manager.merge(templo);
+        manager.getTransaction().commit();
+        
+    }
+    
+    public void excluir(Templo templo) throws SQLException {
+        EntityManager manager = ConexaoBD.getInstance()
+                .getEntityManager();
+        
+        manager.getTransaction().begin();
+        manager.remove(manager.getReference(Templo.class, 
+                templo.getTemploId()));
+        manager.getTransaction().commit();
+        
+        
+    }
+    
+   
+    
+   
     
 }
