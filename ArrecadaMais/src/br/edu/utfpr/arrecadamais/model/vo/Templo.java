f5dfd0aa050dfd3e650336e1b93f455747fe3a26
jiboli <joao.riboli@outlook.com>
2014-10-01 21:29:30 -0300

    Templo

--- ArrecadaMais/src/br/edu/utfpr/arrecadamais/model/vo/Templo.java
+++ ArrecadaMais/src/br/edu/utfpr/arrecadamais/model/vo/Templo.java
@@ -6,13 +6,22 @@
 
 package br.edu.utfpr.arrecadamais.model.vo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author JoãoHenrique
  */
+@Entity
+@Table(name = "Templos")
 public class Templo {
     
-    private int codTemplo;
+    @Id
+    @GeneratedValue
+    private int TemploId;
     private String NomeTemplo;
     private Cidade cidade;
     private Estado estado;
@@ -20,9 +29,74 @@ public class Templo {
     private int numero;
     private int capacidade;
     
+    public Templo(){
+        this.setTemploId(0);
+        this.setNomeTemplo("");
+        this.setCidade(new Cidade());
+        this.setEstado(new Estado());
+        this.setRua("");
+        this.setNumero(0);
+        this.setCapacidade(0);
+        
+    }
+
+    public void setTemploId(int TemploId) {
+        this.TemploId = TemploId;
+    }
+    
 
     public int getTemploId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.TemploId;
     }
+
+    public String getNomeTemplo() {
+        return NomeTemplo;
+    }
+
+    public void setNomeTemplo(String NomeTemplo) {
+        this.NomeTemplo = NomeTemplo;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    
     
 }
